4977c4f71e1c09f26f2523171b4cc471f8dfdee6
Hyeok_Choi <love72huk@naver.com>
2023-10-20 21:02:24 +0900

    init project

---
+++ src/test/java/aplus/insurancesystem2/InsuranceSystem2ApplicationTests.java
@@ -0,0 +1,13 @@
+package aplus.insurancesystem2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceSystem2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
