3cb572e09629a2868bcc9b0918c59763152f7898
Hyeok_Choi <love72huk@naver.com>
2023-10-21 00:40:11 +0900

    feature: Customer 도메인 추가

---
+++ src/main/java/aplus/insurancesystem2/customer/domain/Customer.java
@@ -0,0 +1,35 @@
+package aplus.insurancesystem2.customer.domain;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Customer {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    @Column(name = "customerID")
+    private String customerId;
+
+    private String address;
+    private String customerName;
+    private String job;
+    private String pnumber;
+    private String birth; // 생년월일(yyyy-mm-dd, String)
+    @Enumerated(EnumType.STRING)
+    private EGender eGender; // 성별
+}
