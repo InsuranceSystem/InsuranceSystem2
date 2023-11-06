a596374ce5231f9fab07e6715c0a7a779c1a4c49
Hyeok_Choi <love72huk@naver.com>
2023-11-07 01:05:52 +0900

    refactor: 보험 api path 변경

--- src/main/java/aplus/insurancesystem2/domain/Insurance/controller/InsuranceController.java
+++ src/main/java/aplus/insurancesystem2/domain/Insurance/controller/InsuranceController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/insurance")
+@RequestMapping("/insurances")
 @RequiredArgsConstructor
 public class InsuranceController {
 
