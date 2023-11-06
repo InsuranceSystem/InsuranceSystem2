cb31731f81e2de062a19e495a38e52759b272941
hongkikii <hyangmeeh@icloud.com>
2023-11-06 18:57:09 +0900

    style: ContractService 메소드 순서 변경

--- src/main/java/aplus/insurancesystem2/domain/contract/service/ContractService.java
+++ src/main/java/aplus/insurancesystem2/domain/contract/service/ContractService.java
@@ -6,11 +6,12 @@ import java.util.List;
 public interface ContractService {
     boolean add(Contract contract);
     List<Contract> retrieve() throws Exception;
+    List<Contract> retrieveCustomerContract(String customerId);
+    public List<String> retrieveCustomerContractStatus(String customerId);
+    List<Contract> getContractByInsuranceID(String insuranceID);
+    List<String> getInsuranceIdFromCustomerId(String customerId);
+    String retrievePremiumById(String selectedCustomerId, String selectedInsuranceId);
     boolean updateCancellation(String customerId, String insuranceId);
     void setResurrectFromCustomer(String customerID);
     void setMaturityFromCustomer(String customerID);
-    List<Contract> retrieveCustomerContract(String customerId);
-    List<String> getInsuranceIdFromCustomerId(String customerId);
-    List<Contract> getContractByInsuranceID(String insuranceID);
-    String retrievePremiumById(String selectedCustomerId, String selectedInsuranceId);
 }
