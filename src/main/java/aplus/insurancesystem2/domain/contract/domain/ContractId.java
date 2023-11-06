be638c627c244aa739163593fa1556507fb894e1
hongkikii <hyangmeeh@icloud.com>
2023-11-06 17:09:46 +0900

    refactor: ContractId 생성자 구현

--- src/main/java/aplus/insurancesystem2/domain/contract/domain/ContractId.java
+++ src/main/java/aplus/insurancesystem2/domain/contract/domain/ContractId.java
@@ -3,11 +3,13 @@ package aplus.insurancesystem2.domain.contract.domain;
 import jakarta.persistence.Id;
 import java.io.Serializable;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @EqualsAndHashCode
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class ContractId implements Serializable {
 
     @Id
