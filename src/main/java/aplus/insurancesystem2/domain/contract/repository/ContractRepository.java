069119981f81f703884c9dd8021351f6cec3ef6a
hongkikii <hyangmeeh@icloud.com>
2023-11-06 16:46:50 +0900

    feat: ContractRepository 구현

--- src/main/java/aplus/insurancesystem2/domain/contract/repository/ContractRepository.java
+++ src/main/java/aplus/insurancesystem2/domain/contract/repository/ContractRepository.java
@@ -3,8 +3,12 @@ package aplus.insurancesystem2.domain.contract.repository;
 
 import aplus.insurancesystem2.domain.contract.domain.Contract;
 import aplus.insurancesystem2.domain.contract.domain.ContractId;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ContractRepository extends JpaRepository<Contract, ContractId> {
 
+    List<Contract> findByCustomerID(String customerId);
+
+    List<Contract> findByInsuranceID(String insuranceId);
 }
