f7795d263c410da8b8f1401ca578976e52d01371
Hyeok_Choi <love72huk@naver.com>
2023-11-23 18:04:23 +0900

    feature: 나머지 엔티티 전부 생성

---
+++ src/main/java/aplus/insurancesystem2/domain/compensation/entity/Survey.java
@@ -0,0 +1,51 @@
+package aplus.insurancesystem2.domain.compensation.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * CREATE TABLE Survey (
+ *   CCID BIGINT PRIMARY KEY,
+ *   managerName VARCHAR(255),
+ *   reportFilePath VARCHAR(255),
+ *   surveyFee INT,
+ *   decisionMoney INT,
+ *   responsibility BOOL,
+ *   responsibilityReason VARCHAR(255),
+ *   FOREIGN KEY (CCID) REFERENCES CompensationClaim(CCID)
+ * );
+ */
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Survey {
+
+    @Id
+    @Column(name = "CCID")
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "CCID")
+    private CompensationClaim compensationClaim;
+
+    private String managerName;
+    private String reportFilePath;
+    private Integer surveyFee;
+    private Integer decisionMoney;
+    private Boolean responsibility;
+    private String responsibilityReason;
+
+
+
+
+}
