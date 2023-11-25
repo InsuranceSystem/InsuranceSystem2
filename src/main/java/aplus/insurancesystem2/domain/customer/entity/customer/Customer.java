bf93a52f1df1a4a746cec18c67b3334245a0dcb4
Hyeok_Choi <love72huk@naver.com>
2023-11-25 23:46:36 +0900

    refactor: Customer soft delete 도입

--- src/main/java/aplus/insurancesystem2/domain/customer/entity/customer/Customer.java
+++ src/main/java/aplus/insurancesystem2/domain/customer/entity/customer/Customer.java
@@ -1,11 +1,17 @@
 package aplus.insurancesystem2.domain.customer.entity.customer;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,6 +20,8 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Where(clause = "deletedAt is null")
+@SQLDelete(sql = "UPDATE Customer SET deletedAt = current_timestamp WHERE customerID = ?")
 public class Customer {
 
     @Id
@@ -29,4 +37,18 @@ public class Customer {
     private String pnumber;
     private String birth; // 생년월일(yyyy-mm-dd, String)
     private EGender eGender; // 성별
+    private LocalDateTime deletedAt;
+
+    @Builder
+    public Customer(String customerId, String address, String customerName, String job, String pnumber,
+                    String birth, EGender eGender) {
+        this.customerId = customerId;
+        this.address = address;
+        this.customerName = customerName;
+        this.job = job;
+        this.pnumber = pnumber;
+        this.birth = birth;
+        this.eGender = eGender;
+        this.deletedAt = null;
+    }
 }
