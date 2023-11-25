f67c6a92d53560410a14a13f9fc9453775689978
Hyeok_Choi <love72huk@naver.com>
2023-11-25 10:42:48 +0900

    refactor: TermsController 삭제

--- src/main/java/aplus/insurancesystem2/domain/Insurance/service/TermsService.java
+++ src/main/java/aplus/insurancesystem2/domain/Insurance/service/TermsService.java
@@ -2,13 +2,8 @@ package aplus.insurancesystem2.domain.Insurance.service;
 
 import java.util.List;
 
-import aplus.insurancesystem2.domain.Insurance.dto.request.termsCreateRequest;
 import aplus.insurancesystem2.domain.Insurance.dto.response.TermInfoResponse;
-import aplus.insurancesystem2.domain.Insurance.entity.Terms;
 
 public interface TermsService {
-    List<Terms> getTermsList();
-    String createTerms(termsCreateRequest terms);
-
     List<TermInfoResponse> getInsuranceTerms(Long insuranceId);
 }
--- src/main/java/aplus/insurancesystem2/domain/Insurance/service/TermsServiceImpl.java
+++ src/main/java/aplus/insurancesystem2/domain/Insurance/service/TermsServiceImpl.java
@@ -5,29 +5,16 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import aplus.insurancesystem2.domain.Insurance.dto.request.termsCreateRequest;
 import aplus.insurancesystem2.domain.Insurance.dto.response.TermInfoResponse;
-import aplus.insurancesystem2.domain.Insurance.entity.Terms;
 import aplus.insurancesystem2.domain.Insurance.repository.TermsRepository;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
-public class TermsServiceImpl implements TermsService{
+public class TermsServiceImpl implements TermsService {
+
     private final TermsRepository termsRepository;
-
-    @Override
-    public List<Terms> getTermsList() {
-        return termsRepository.findAll();
-    }
-
-    @Override
-    public String createTerms(termsCreateRequest termsDto){
-        Terms terms = new Terms(termsDto);
-        termsRepository.save(terms);
-        return "Success";
-    }
 
     @Override
     public List<TermInfoResponse> getInsuranceTerms(Long insuranceId) {
