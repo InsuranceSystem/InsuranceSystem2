1116516b8801bb9f455ce7576332724ca536fcd9
Hyeok_Choi <love72huk@naver.com>
2023-12-02 03:00:13 +0900

    refactor: security 설정 변경

--- src/main/java/aplus/insurancesystem2/common/config/SecurityConfig.java
+++ src/main/java/aplus/insurancesystem2/common/config/SecurityConfig.java
@@ -12,12 +12,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import aplus.insurancesystem2.common.security.AplusAccessDeniedHandler;
+import aplus.insurancesystem2.common.security.AplusAuthenticationFailureHandler;
 import aplus.insurancesystem2.common.security.AplusAuthenticationSuccessHandler;
 
 @Configuration
@@ -32,6 +34,11 @@ public class SecurityConfig {
     @Bean
     public SimpleUrlAuthenticationSuccessHandler loginSuccessHandler() {
         return new AplusAuthenticationSuccessHandler();
+    }
+
+    @Bean
+    public AuthenticationFailureHandler loginFailureHandler() {
+        return new AplusAuthenticationFailureHandler();
     }
 
     @Bean
@@ -62,6 +69,7 @@ public class SecurityConfig {
                         .usernameParameter("loginId")
                         .passwordParameter("password")
                         .successHandler(loginSuccessHandler())
+                        .failureHandler(loginFailureHandler())
                 )
                 .exceptionHandling((exception) -> exception
                         .accessDeniedHandler(accessDeniedHandler())
--- src/main/java/aplus/insurancesystem2/common/security/AplusAuthenticationSuccessHandler.java
+++ src/main/java/aplus/insurancesystem2/common/security/AplusAuthenticationSuccessHandler.java
@@ -10,19 +10,10 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public class AplusAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
-//    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws
                                                                                                                                  IOException {
-//        SavedRequest save = (SavedRequest) request.getSession().getAttribute("SPRING_SECURITY_SAVED_REQUEST");
-//        System.out.println("save = " + save);
-//        String uri = save != null ? save.getRedirectUrl() : "/";
-//        System.out.println("uri = " + uri);
-//        redirectStrategy.sendRedirect(request, response, uri);
-//        Enumeration<String> list = request.getSession().getAttributeNames();
-//        while (list.hasMoreElements()) {
-//            System.out.println(list.nextElement());
-//        }
+        // do nothing
     }
 }
