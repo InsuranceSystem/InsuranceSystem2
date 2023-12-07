f8ba380f6ade8c32c552cd70c0d5c160ea342f0a
ibaesuyeon <ibaesuyeon@Naver.com>
2023-12-07 15:02:12 +0900

    refactor: 파라미터 아이디명 변경
    customerId에서 contractId로 변경

--- src/main/java/aplus/insurancesystem/domain/compensationClaim/repository/CompensationClaimRepository.java
+++ src/main/java/aplus/insurancesystem/domain/compensationClaim/repository/CompensationClaimRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CompensationClaimRepository extends JpaRepository<CompensationClaim, Long> {
-    List<CompensationClaim> findAllByContractId(Long customerId);
+    List<CompensationClaim> findAllByContractId(Long contractId);
 }
